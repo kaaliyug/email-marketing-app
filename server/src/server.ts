30b139ba25243df091e4d29954bb8cd7ba83ca56
Satyaki <satyakibasua08@gmail.com>
2026-07-22 09:28:19 +0530

    Prepare backend for Render deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,10 @@ import app from "./app.js";
 import "./workers/campaign.worker.js";
 import { recoverScheduledCampaigns } from "./services/scheduleRecovery.service.js";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
-    console.log(`Server running on port ${PORT}`);
-   await recoverScheduledCampaigns();
+  console.log(`Server running on port ${PORT}`);
 
+  await recoverScheduledCampaigns();
 });
