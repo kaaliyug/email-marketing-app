b09ba9b0f1e100f4faa8eef695955b29dabe7c1f
Satyaki <satyakibasua08@gmail.com>
2026-07-22 09:11:09 +0530

    Use REDIS_URL for Upstash Redis

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,17 +1,15 @@
 import { Redis } from "ioredis";
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: Number(process.env.REDIS_PORT) || 6379,
+const redis = new Redis(process.env.REDIS_URL!, {
+  maxRetriesPerRequest: null,
+});
 
-    // Required by BullMQ
-    maxRetriesPerRequest: null,
-});
 redis.on("connect", () => {
-    console.log("✅ Redis Connected");
+  console.log("✅ Redis Connected");
 });
+
 redis.on("error", (err: Error) => {
-    console.error("❌ Redis Error:", err.message);
+  console.error("❌ Redis Error:", err.message);
 });
 
 export default redis;
